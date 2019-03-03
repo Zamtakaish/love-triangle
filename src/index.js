68a4752c9005d1f1f0926a2c6dceebd663e704f4
Anton Ziuzikov <zuzikov.anton@gmail.com>
2019-03-03 22:03:07 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+          var count = 0;
+        for(var i = 0; i < preferences.length; i++) {
+            if ((preferences[preferences[i] - 1] !== (i + 1))&&(preferences[preferences[i] - 1] !== "triangle")){
+                var first = i;
+                var second  = preferences[first] - 1;
+                var third = preferences[second] - 1;
+                if ((preferences[third] - 1) === first){
+                    preferences[first] = "triangle";
+                    preferences[second] = "triangle";
+                    preferences[third] = "triangle";
+                    count++;
+                }
+            }
+        }
+    return count;
 };
